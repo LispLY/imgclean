b1a61c9b2ab0af61a25935d002bb7dc88319e2b2
Louie Lu <lisply@gmail.com>
2019-07-21 16:17:28 +0800

    init

---
+++ imgclean/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  imgclean
+//
+//  Created by Apple on 2019/7/21.
+//  Copyright © 2019 Thorough Images. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
